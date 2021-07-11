02d5368a999392384ef5a9637b6eb358fb39eff3
sein0211 <sein4037@gmail.com>
2021-07-11 10:58:31 +0900

    first

---
+++ src/test/java/com/server/tourApiProject/TourApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.tourApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
