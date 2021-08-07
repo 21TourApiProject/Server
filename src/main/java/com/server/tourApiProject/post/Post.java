a4d94b753eab43d3c833def9aac4f0531eeea398
sein0211 <sein4037@gmail.com>
2021-08-07 17:26:52 +0900

    게시물

--- src/main/java/com/server/tourApiProject/post/Post.java
+++ src/main/java/com/server/tourApiProject/post/Post.java
@@ -2,27 +2,17 @@ package com.server.tourApiProject.post;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.server.tourApiProject.hashTag.HashTag;
 import com.server.tourApiProject.postHashTag.PostHashTag;
 import com.server.tourApiProject.postImage.PostImage;
 import com.server.tourApiProject.user.User;
-
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.awt.*;
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.*;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Builder
 @Getter
