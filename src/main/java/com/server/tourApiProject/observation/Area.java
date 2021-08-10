4071c9e18f0b47edf670648310fedf992504f922
sein0211 <sein4037@gmail.com>
2021-08-10 22:50:39 +0900

    관광지 open api

---
+++ src/main/java/com/server/tourApiProject/observation/Area.java
@@ -0,0 +1,30 @@
+package com.server.tourApiProject.observation;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="area")
+public class Area {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long areaCode; //지역코드
+
+    @Column(nullable = false)
+    private String areaName; //지역이름
+
+    @Column
+    private Long sigunguCode; //시군구코드
+
+    @Column
+    private String sigunguName; //시군구이름
+}
---
+++ src/main/java/com/server/tourApiProject/observation/AreaParams.java
@@ -0,0 +1,13 @@
+package com.server.tourApiProject.observation;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AreaParams {
+    Long code;
+    String name;
+}
