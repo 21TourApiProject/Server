cbcae6b3d00fe896b0cd71dec78ee47a370a9caf
dingding-21 <phj8221@naver.com>
2021-08-19 11:06:38 +0900

    별자리 id로 별자리 검색

--- src/main/java/com/server/tourApiProject/constellation/ConstellationController.java
+++ src/main/java/com/server/tourApiProject/constellation/ConstellationController.java
@@ -4,10 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.jni.Local;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -20,20 +18,26 @@ public class ConstellationController {
 
     @ApiOperation(value = "별자리 입력", notes = "별자리 정보를 입력한다.")
     @PostMapping(value = "constellation")
-    public void createConstellation(@RequestBody Constellation constellation){
+    public void createConstellation(@RequestBody Constellation constellation) {
         constellationService.createConstellation(constellation);
     }
 
     @ApiOperation(value = "모든 별자리 조회", notes = "모든 별자리를 조회한다")
     @GetMapping(value = "constellations")
-    public List<Constellation> getConstellation(){
-        return constellationService.getAllConstellation();
+    public List<Constellation> getConstellation() {
+        return constellationService.getConstellation();
     }
 
     @ApiOperation(value = "당일 날짜에 보이는 별자리 조회", notes = "당일 날짜에 보이는 별자리를 조회한다")
     @GetMapping(value = "constellation/todayConst")
-    public List<ConstellationParams> getTodayConst(){
+    public List<ConstellationParams> getTodayConst() {
         return constellationService.getTodayConst();
+    }
+
+    @ApiOperation(value = "별자리 상세 정보 조회", notes = "졀자리 id로 별자리 상세 정보를 조회한다.")
+    @GetMapping(value = "constellation/{constId}")
+    public Constellation getDetailConst(@PathVariable("constId") Long constId) {
+        return constellationService.getDetailConst(constId);
     }
 
 }
--- src/main/java/com/server/tourApiProject/constellation/ConstellationService.java
+++ src/main/java/com/server/tourApiProject/constellation/ConstellationService.java
@@ -1,12 +1,7 @@
 package com.server.tourApiProject.constellation;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.key.LocalDateKeyDeserializer;
-import com.server.tourApiProject.bigPost.post.Post;
-import com.server.tourApiProject.myWishPost.MyWishPost;
-import com.server.tourApiProject.myWishPost.MyWishPostParams;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -21,20 +16,20 @@ import java.util.List;
 public class ConstellationService {
     private final ConstellationRepository constellationRepository;
 
-    public List<Constellation> getAllConstellation() {
-        return constellationRepository.findAll();
-    }
-
     public void createConstellation(Constellation constellation) {
         constellationRepository.save(constellation);
     }
 
+    public List<Constellation> getConstellation() {
+        return constellationRepository.findAll();
+    }
+
     public List<ConstellationParams> getTodayConst() {
-        List<ConstellationParams> result= new ArrayList<>();
+        List<ConstellationParams> result = new ArrayList<>();
         LocalDate currentDate = LocalDate.now();
         List<Constellation> list = constellationRepository.findByStartDateLessThanEqualAndEndDateGreaterThanEqual(currentDate, currentDate);
 
-        for(Constellation cl : list){
+        for (Constellation cl : list) {
             Constellation constellation = constellationRepository.findById(cl.getConstId()).orElseThrow(IllegalAccessError::new);
 
             ConstellationParams params = new ConstellationParams();
@@ -45,20 +40,10 @@ public class ConstellationService {
         }
         return result;
     }
+
+    public Constellation getDetailConst(Long constId) {
+        Constellation constellation = constellationRepository.findById(constId).orElseThrow(IllegalAccessError::new);
+        return constellation;
+    }
 }
 
-//    public List<MyWishPostParams> getMyWishPosts(Long userId) {
-//        List<MyWishPostParams> result= new ArrayList<>();
-//
-//        List<MyWishPost> list = myWishPostRepository.findByUserId(userId);
-//        for (MyWishPost wp : list){
-//            Post post = postRepository.findById(wp.getPostId()).orElseThrow(IllegalAccessError::new);
-//
-//            MyWishPostParams params = new MyWishPostParams();
-//            params.setPostId(post.getPostId());
-//            params.setThumbnail(""); //추후 첫번째 이미지로 수정
-//            params.setTitle(post.getPostContent()); //추후 제목으로 수정
-//            result.add(params);
-//        }
-//        return result;
-//    }
