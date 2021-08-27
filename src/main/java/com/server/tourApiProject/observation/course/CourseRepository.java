22618509b3f0d5ffd9b8cc86d6c3a035e93115e5
gyulchyoung <hacha2011@naver.com>
2021-08-27 23:14:06 +0900

    change observation entrance fee

---
+++ src/main/java/com/server/tourApiProject/observation/course/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.server.tourApiProject.observation.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
