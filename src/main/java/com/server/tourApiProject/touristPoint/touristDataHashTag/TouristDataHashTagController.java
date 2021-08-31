04f60a80eecc84dd8b0ecbc77182dfd09773a8d2
sein0211 <sein4037@gmail.com>
2021-08-31 13:02:27 +0900

    관측지 해시태그

--- src/main/java/com/server/tourApiProject/touristPoint/touristDataHashTag/TouristDataHashTagController.java
+++ src/main/java/com/server/tourApiProject/touristPoint/touristDataHashTag/TouristDataHashTagController.java
@@ -4,10 +4,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import retrofit2.http.Path;
+
+import java.util.List;
 
 @Slf4j
 @Api(tags = {"5.5 관광지-해시태그"})
@@ -18,9 +18,9 @@ public class TouristDataHashTagController {
 
     private final TouristDataHashTagService touristDataHashTagService;
 
-    @ApiOperation(value = "관광지 해시태그 입력", notes = "관광지 해시태그 정보를 입력한다")
-    @PostMapping(value = "touristDataHashTag")
-    public void createTouristDataHashTag(@RequestBody TouristDataHashTag touristDataHashTag){
-        touristDataHashTagService.createTouristDataHashTag(touristDataHashTag);
+    @ApiOperation(value = "관광지 해시태그 조희", notes = "관광지 해시태그 정보를 조희한다")
+    @GetMapping(value = "touristDataHashTag/{contentId}")
+    public List<String> getTouristDataHashTag(@PathVariable("contentId") Long contentId){
+        return touristDataHashTagService.getTouristDataHashTag(contentId);
     }
 }
