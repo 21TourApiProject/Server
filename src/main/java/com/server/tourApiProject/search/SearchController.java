bca12ae2ebdb78a76814182396b53c4e6bd4ee6c
parkjinhyeok348 <wlsgur348@gmail.com>
2021-10-09 15:13:33 +0900

    Post search commit

--- src/main/java/com/server/tourApiProject/search/SearchController.java
+++ src/main/java/com/server/tourApiProject/search/SearchController.java
@@ -1,5 +1,7 @@
 package com.server.tourApiProject.search;
 
+import com.server.tourApiProject.bigPost.post.PostParams6;
+import com.server.tourApiProject.bigPost.post.PostService;
 import com.server.tourApiProject.observation.ObservationService;
 import com.server.tourApiProject.touristPoint.touristData.TouristDataService;
 import io.swagger.annotations.Api;
@@ -22,6 +24,7 @@ public class SearchController {
 
     private final ObservationService observationService;
     private final TouristDataService touristDataService;
+    private final PostService postService;
 
     @ApiOperation(value = "관측지 검색결과 ", notes = "검색어와 필터로 관측지 검색결과를 조회한다")
     @PostMapping(value = "search/observation")
@@ -34,4 +37,9 @@ public class SearchController {
     public List<SearchParams1> getTouristPointWithFilter(@RequestBody SearchKey searchKey){
         return touristDataService.getTouristPointWithFilter(searchKey.getFilter(), searchKey.getKeyword());
     }
+    @ApiOperation(value = "게시물 정보 필터로 조회", notes = "필터로 걸러진 게시물을 조회한다")
+    @PostMapping(value = "search/post")
+    public List<PostParams6> getPostWithFilter(@RequestBody SearchKey searchKey){
+        return postService.getPostDataWithFilter(searchKey.getFilter(),searchKey.getKeyword()); }
+
 }
