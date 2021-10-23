6724e7a04681c1ca1d3e45395dcbfe79f46630c4
dingding-21 <phj8221@naver.com>
2021-10-24 00:27:46 +0900

    별자리 table 수정 및 해당 달로 리스트 받아오기

--- src/main/java/com/server/tourApiProject/star/Horoscope/HoroscopeController.java
+++ src/main/java/com/server/tourApiProject/star/Horoscope/HoroscopeController.java
@@ -19,10 +19,10 @@ import java.util.List;
 public class HoroscopeController {
     private final HoroscopeService horoscopeService;
 
-    @ApiOperation(value = "id로 별자리 정보 조회", notes = "id로 별자리 정보를 조회한다")
-    @GetMapping(value = "horoscope/{horId}")
-    public Horoscope getHoroscopes(@PathVariable("horId") Long horId) {
-        return horoscopeService.getHoroscope(horId);
+    @ApiOperation(value = "당일 날짜의 달로 별자리 정보 조회", notes = "당일 날짜의 달로 별자리 정보를 조회한다")
+    @GetMapping(value = "horoscope/{todayMonth}")
+    public List<HoroscopeParams> getHoroscopes(@PathVariable("todayMonth") int todayMonth) {
+        return horoscopeService.getHoroscopes(todayMonth);
     }
 
     @ApiOperation(value = "모든 별자리 운세 조회", notes = "모든 별자리 운세를 조회한다")
--- src/main/java/com/server/tourApiProject/star/constellation/Constellation.java
+++ src/main/java/com/server/tourApiProject/star/constellation/Constellation.java
@@ -22,35 +22,20 @@ public class Constellation {
     @Column(nullable = false)
     private String constName;  //별자리 이름
 
-//    @Column(nullable = false)
-//    private String constBigImage;  //별자리 큰 이미지
-//
-//    @Column(nullable = false)
-//    private String constSmallImage;  //별자리 작은 이미지
-
     @Column(nullable = false, columnDefinition = "TEXT")
     private String constStory;    // 별자리 설화
 
     @Column(nullable = false, columnDefinition = "TEXT")
     private String constMtd;    // 별자리 관측법
 
-    @Column(columnDefinition = "TEXT")
-    private String constTravel; // 여행 취향
-
     @Column(nullable = false)
     private String constBestMonth; // 가장 보기 좋은 달
-
-    @Column(columnDefinition = "TEXT")
-    private String constPersonality; // 별자리 성격
-    private String constPeriod; // 별자리 기간(성격에 포함됨)
 
     private String constFeature1; // 별자리 특징(배너 형식)
 
     private String constFeature2; // 별자리 특징(배너 형식)
 
     private String constFeature3; // 별자리 특징(배너 형식)
-
-    private String constGuard; // 수호성
 
     @Column(nullable = false)
     @DateTimeFormat(pattern = "MM-dd")
@@ -72,13 +57,4 @@ public class Constellation {
 
     @Column(nullable = false)
     private String constEng;  //별자리 이름
-
-
-//    @DateTimeFormat(pattern = "MM-dd")
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd", timezone = "Asia/Seoul")
-//    private String startYear;    // 01-01
-//
-//    @DateTimeFormat(pattern = "MM-dd")
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd", timezone = "Asia/Seoul")
-//    private String endYear;    // 12-31
 }
