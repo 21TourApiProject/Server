a4b62d6c68e52d7bff9e00906cfd8f4790a88aee
parkjinhyeok348 <wlsgur348@gmail.com>
2021-10-31 22:44:08 +0900

    게시물 정렬 commit

--- src/main/java/com/server/tourApiProject/bigPost/post/PostService.java
+++ src/main/java/com/server/tourApiProject/bigPost/post/PostService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -270,6 +271,7 @@ public class PostService {
                 result.add(postParams4);
             }
         }
+        result.sort(Collections.reverseOrder());
             return result;
         }
 
